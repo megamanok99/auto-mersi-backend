7a68787ba23d448e54c1ba4679ddf30d6e9f16d0
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2023-10-08 01:13:38 +0300

    2

---
+++ index.js
@@ -0,0 +1,39 @@
+import cors from 'cors';
+import express from 'express';
+import nodemailer from 'nodemailer';
+const app = express();
+app.use(cors());
+
+let transporter = nodemailer.createTransport({
+  host: 'mail.hostland.ru',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+let mailOptions = {
+  from: 'sale@auto-mersi.ru',
+  to: 'megamanok99@gmail.com',
+  subject: 'Прикинь,это отправлено с почтового сервера',
+  text: 'оно работает',
+};
+
+app.get('/', (req, res) => {
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('here');
+      return console.log(error);
+    }
+    console.log('Сообщение отправлено: %s', info.messageId);
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log('running server');
+  }
+});
